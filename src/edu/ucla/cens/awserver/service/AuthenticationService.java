73955ae34ef5bac9a9957c487078d62b9e7ba8b6
selsky <selsky@0fff1121-3ca1-4544-9340-b5545a1c01e9>
2010-03-12 17:50:37 +0000

    now sets the campaign-roles on a user post-login

--- src/edu/ucla/cens/awserver/service/AuthenticationService.java
+++ src/edu/ucla/cens/awserver/service/AuthenticationService.java
@@ -12,7 +12,7 @@ import edu.ucla.cens.awserver.util.StringUtils;
 import edu.ucla.cens.awserver.validator.AwRequestAnnotator;
 
 /**
- * Dispatches to a DAO to perform authentication checks.
+ * Service for authenticating users via phone or web page.
  * 
  * @author selsky
  */
@@ -20,16 +20,22 @@ public class AuthenticationService extends AbstractAnnotatingDaoService {
 	private static Logger _logger = Logger.getLogger(AuthenticationService.class);
 	private String _errorMessage;
 	private String _disabledMessage;
+	private String _newAccountMessage;
+	private boolean _newAccountsAllowed;
 	
 	/**
-	 * Creates an instance of this class using the supplied DAO as the method of data access. The error message and the data 
-	 * message are set through this constructor in order to customize the output depending on the context in which this 
-	 * component is used (i.e., is the response being sent to a web page or a phone). 
+	 * Creates an instance of this class using the supplied DAO as the method of data access. The *Message parameters are set 
+	 * through this constructor in order to customize the output depending on the context in which this component is used  
+	 * (i.e., is login occuring through a web page or a phone). The newAccountsAllowed parameter specifies whether this 
+	 * service will treat new accounts as successful logins. New accounts are allowed access for the intial login via a phone and
+	 * blocked in every other case.
 	 * 
 	 * @throws IllegalArgumentException if errorMessage is null, empty, or all whitespace 
 	 * @throws IllegalArgumentException if disabledMessage is null, empty, or all whitespace
 	 */
-	public AuthenticationService(Dao dao, AwRequestAnnotator awRequestAnnotator, String errorMessage, String disabledMessage) {
+	public AuthenticationService(Dao dao, AwRequestAnnotator awRequestAnnotator, String errorMessage, String disabledMessage, 
+		String newAccountMessage, boolean newAccountsAllowed) {
+		
 		super(dao, awRequestAnnotator);
 		if(StringUtils.isEmptyOrWhitespaceOnly(errorMessage)) {
 			throw new IllegalArgumentException("an error message is required");
@@ -37,8 +43,14 @@ public class AuthenticationService extends AbstractAnnotatingDaoService {
 		if(StringUtils.isEmptyOrWhitespaceOnly(disabledMessage)) {
 			throw new IllegalArgumentException("a disabled message is required");
 		}
+		if(StringUtils.isEmptyOrWhitespaceOnly(newAccountMessage)) {
+			_logger.info("configured without a new account message");
+		}
+		
 		_errorMessage = errorMessage;
 		_disabledMessage = disabledMessage;
+		_newAccountMessage = newAccountMessage;
+		_newAccountsAllowed = newAccountsAllowed;
 	}
 	
 	/**
@@ -53,54 +65,78 @@ public class AuthenticationService extends AbstractAnnotatingDaoService {
 			// the results the DAO returns
 			getDao().execute(awRequest);
 			
-			// A List is returned from the DAO even though our login ids are unique in the db.
-			// List<?> results = (List<?>) awRequest.getAttribute("results");
 			List<?> results = awRequest.getResultList();
 			
 			if(null != results && results.size() > 0) {
-			
-				// This check is perfunctory, but indicates a serious problem because the condition can only occur if there is more 
-				// than one user with the same id, which is something enforced in the data world.
-				if(results.size() > 1) {
-					throw new ServiceException("more than one user returned for user name (user.login_id) " + awRequest.getUser().getUserName());
-				}
 				
-				LoginResult loginResult = (LoginResult) results.get(0);
+				for(int i = 0; i < results.size(); i++) {
 				
-				// Determine if the user is enabled and not "new" (i.e., allowed to access the application). New users are users
-				// that have not created their own password from a phone
-				if(loginResult.isEnabled() && ! loginResult.isNew()) {
+					LoginResult loginResult = (LoginResult) results.get(i);
 					
-					awRequest.getUser().setCampaignId(loginResult.getCampaignId()); // eventually users may belong to more than one campaign
-					awRequest.getUser().setId(loginResult.getUserId());
-					awRequest.getUser().setLoggedIn(true);
-					
-					// remove the password from the user object just to be on the safe side (it is not needed again anyway)
-					awRequest.getUser().setPassword(null);
-					
-					_logger.info("user " + awRequest.getUser().getUserName() + " successfully logged in");
-				
-				} else {
-					
-					getAnnotator().annotate(awRequest, _disabledMessage);
-					if(! loginResult.isEnabled()) {
-						_logger.info("user " + awRequest.getUser().getUserName() + " is not enabled for access");
-					} 
-					if(loginResult.isNew()) {
+					if(! _newAccountsAllowed && loginResult.isNew()) {
+						
+						getAnnotator().annotate(awRequest, _newAccountMessage);
 						_logger.info("user " + awRequest.getUser().getUserName() + " is new and must change their password via " +
 							"phone before being granted access");
+						
+						return;
 					}
+					
+					if(! loginResult.isEnabled()) {
+						
+						getAnnotator().annotate(awRequest, _disabledMessage);
+						_logger.info("user " + awRequest.getUser().getUserName() + " is not enabled for access");
+						
+						return;
+						
+					}
+					
+					if(0 == i) { // first time thru: grab the properties that are common across all LoginResults (i.e., data from
+						         // the user table)
+						
+						awRequest.getUser().setId(loginResult.getUserId());
+						awRequest.getUser().setLoggedIn(true);
+					}
+					
+					// set the campaigns and the roles within the campaigns that the user belongs to
+					awRequest.getUser().addCampaignRole(loginResult.getCampaignId(), loginResult.getUserRoleId());
 				}
 				
-			} else { // no user found
+				// Set the current campaign on the user if the user belongs to only one campaign. If the user belongs to more
+				// than one campaign, he or she will have to choose a campaign post-login.
+				if(userBelongsToOneCampaign(results)) {
+					awRequest.getUser().setCurrentCampaignId(((LoginResult)results.get(0)).getCampaignId());
+				}
+				
+				
+				_logger.info("user " + awRequest.getUser().getUserName() + " successfully logged in");
+				
+			} else { // no user found or invalid password
 				
 				getAnnotator().annotate(awRequest, _errorMessage);
-				_logger.info("user " + awRequest.getUser().getUserName() + " not found");
+				_logger.info("user " + awRequest.getUser().getUserName() + " not found or invalid password was supplied");
 			}
 			
 		} catch (DataAccessException dae) { 
 			
 			throw new ServiceException(dae);
 		}
+	}
+	
+	/**
+	 * Check to see whether the results list contains multiple campaigns. 
+	 */
+	private boolean userBelongsToOneCampaign(List<?> results) {
+		if(results.size() == 1) {
+			return true;
+		} else {
+			int cid = ((LoginResult) results.get(0)).getCampaignId();
+			for(int i = 1; i < results.size(); i++) {
+				if(cid != ((LoginResult) results.get(i)).getCampaignId()) {
+					return false;
+				}
+			}
+		}
+		return true;
 	}
 }
