084336523cd95525499cb2fc82df2794a0719fe2
selsky <selsky@0fff1121-3ca1-4544-9340-b5545a1c01e9>
2010-03-12 19:28:44 +0000

    drop subdomain column from campaign table, change label for pilot study

--- database/alter-tables-r1-ddl.sql
+++ database/alter-tables-r1-ddl.sql
@@ -6,5 +6,16 @@ USE andwellness;
 -- --------------------------------------------------
 ALTER TABLE user 
 ADD COLUMN password varchar(100) NOT NULL,
-ADD COLUMN new_account bit NOT NULL
+ADD COLUMN new_account bit NOT NULL;
 
+-- --------------------------------------------------
+-- Removing subdomains from app
+-- --------------------------------------------------
+ALTER TABLE campaign
+DROP COLUMN subdomain;
+
+-- --------------------------------------------------
+-- Give the initial pilot campaign a better label
+-- --------------------------------------------------
+
+UPDATE campaign SET label = 'Breast Cancer Survivor Study' WHERE id = 1;
