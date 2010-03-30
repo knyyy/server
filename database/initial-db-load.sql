95e3fbc2a90ac4ba1f22ae62a1b46f515b755aa8
selsky <selsky@0fff1121-3ca1-4544-9340-b5545a1c01e9>
2010-03-30 21:10:09 +0000

    changes for latest prompt spec updates

--- database/initial-db-load.sql
+++ database/initial-db-load.sql
@@ -18,17 +18,18 @@ INSERT INTO campaign_prompt_group (campaign_id, campaign_prompt_version_id, grou
 INSERT INTO prompt_type (type, restriction) VALUES
   ("time_military", NULL),
   ("array_boolean", "3"),
-  ("map", '{0:"10",1:"20",2:"30",3:"40",4:"50",5:"60+"}'),
-  ("map", '{0:"<4",1:"5",2:"6",3:"7",4:"8",5:">8"}'),
-  ("map", '{0:"very bad",1:".",2:".",3:"very good"}'),
-  ("map", '{0:"not at all",1:".",2:"slightly",3:".",4:"moderately",5:".",6:"extremely"}'),
-  ("map", '{0:"never",1:"almost never",2:"sometimes",3:"fairly often",4:"very often"}'),
+  ("map", '{0:"<10",1:"10-20",2:"20-30",3:"30-40",4:"50+"}'),
+  ("map", '{0:0,1:1,2:2,3:3,4:4,5:5,6:6,7:7,8:8,9:9,10:10,11:11,12:12}'),
+  ("map", '{0:"very bad",1:"bad",2:"good",3:"very good"}'),
+  ("map", '{0:"not at all",1:"slightly",2:"moderately",3:"extremely"}'),
+  ("map", '{0:"rarely",1:"sometimes",2:"often"}'),
   ("map", '{0:"none",1:"light",2:"moderate",3:"vigorous"}'),
   ("map", '{0:"0",1:"1",2:"2",3:"3",4:"4",5:"5",6:"6",7:"7",8:"8",9:"9",10:"10+"}'),
-  ("map", '{1:"Yes",0:"No"}'),
+  ("map", '{0:"No",1:"Yes"}'),
   ("array_boolean", "6"),
   ("null", NULL),
-  ("map", '{0:"10",1:"20",2:"30",3:"40",4:"50",5:"60+",6:"N/A"}');
+  ("map", '{0:"10",1:"20",2:"30",3:"40",4:"50",5:"60+",6:"N/A"}'), -- FIXME confirm w Nithya
+  ("map", '{0:"not at all",1:"slightly",2:"moderately",3:"very"}');
 
 INSERT INTO prompt (prompt_type_id, campaign_prompt_group_id, campaign_prompt_version_id, prompt_config_id, parent_config_id, question_text, legend_text) VALUES
   (1, 1, 1, 0, NULL, "Take a saliva sample now and enter time.", "saliva"),
@@ -41,20 +42,15 @@ INSERT INTO prompt (prompt_type_id, campaign_prompt_group_id, campaign_prompt_ve
   (5, 2, 1, 4, NULL, "How would you rate your sleep quality?","sleep quality"),
   
   (12, 3, 1, 0, NULL, "For the following, please rate how much you are currently feeling:","currently feeling "),
-  (6, 3, 1, 1, 0, "Sad?","feeling sad"),
-  (6, 3, 1, 2, 0, "Inspired?","feeling inspired"),
-  (6, 3, 1, 3, 0, "Calm?","feeling calm"),
-  (6, 3, 1, 4, 0, "Upset?","feeling upset"),
-  (6, 3, 1, 5, 0, "Nervous?","feeling nervous"),
-  (6, 3, 1, 6, 0, "Energetic?","feeling energetic"),
-  (6, 3, 1, 7, 0, "Happy?","feeling happy"),
-  (6, 3, 1, 8, 0, "Blue?","feeling blue"),
-  (6, 3, 1, 9, 0, "Anxious","feeling anxious"),
-  (6, 3, 1, 10, 0, "Relaxed?","feeling relaxed"),
-  (6, 3, 1, 11, 0, "Tired?","feeling tired"),
-  (6, 3, 1, 12, 0, "Cheerful","feeling cheerful"),
-  (6, 3, 1, 13, 0, "Enthusiastic?","feeling enthusiastic"),
-  (6, 3, 1, 14, 0, "Stressed?","feeling stressed"),
+  (6, 3, 1, 1, 0, "Sad","feeling sad"),
+  (6, 3, 1, 2, 0, "Relaxed","feeling relaxed"),
+  (6, 3, 1, 3, 0, "Anxious","feeling anxious"),
+  (6, 3, 1, 4, 0, "Tired","feeling tired"),
+  (6, 3, 1, 5, 0, "Happy","feeling happy"),
+  (6, 3, 1, 6, 0, "Upset","feeling upset"),
+  (6, 3, 1, 7, 0, "Energetic","feeling energetic"),
+  (6, 3, 1, 8, 0, "Irritable","feeling irritable"),
+  (6, 3, 1, 9, 0, "Calm","feeling calm"),
   
   (12, 4, 1, 0, NULL, "Did you experience any of the following today:","experiences"),
   (10, 4, 1, 1, 0, "Little interest or pleasure in doing things?","little interest or pleasure"),
@@ -65,17 +61,21 @@ INSERT INTO prompt (prompt_type_id, campaign_prompt_group_id, campaign_prompt_ve
   (7, 4, 1, 6, NULL, "How often did you feel that things were going your way today?","things going your way"),
   (7, 4, 1, 7, NULL, "How often did you find that you could not cope with all the things that you had to do today?","cope with things you had to do"),
   (7, 4, 1, 8, NULL, "How often did you feel difficulties were piling up so high that you could not overcome them?","difficulties too high to overcome"),
-  (10, 4, 1, 9, NULL, "Did you plan to exercise today?","did you plan to exercise"),
-  (10, 4, 1, 10, NULL, "Did you exercise today?","did you exercise"),
-  (8, 4, 1, 11, 10, "If yes, what type of exercise did you today?","type of exercise"),
-  (13, 4, 1, 12, 10, "If you exercised, for how many minutes did you exercise?","how many minutes did you exercise"),
-  (11, 4, 1, 13, NULL, "Did any of the following interfere with your plan to exercise today? (lack of time, lack of self-discipline, fatigue, procrastination, lack of interest, family work or responsibilities,","exercise interference"),
-  (9, 4, 1, 14, NULL, "How many cigarettes did you smoke today?","number of cigarettes"),
-  (9, 4, 1, 15, NULL, "How many alcoholic beverages did you have today?","number of alcoholic beverages"),
-  (9, 4, 1, 16, NULL, "How many caffeinated beverages did you have today?","number of caffeinated beverages"),
-  (10, 4, 1, 17, NULL, "Did you have any high sugar food or drinks today? (soft drinks, candy, etc)","sugary food or drinks"),
-  (10, 4, 1, 18, NULL, "Did anything happen today that was stressful or difficult for you?","did anything stressful happen"),
-  (6, 4, 1, 19, 18, "If so, how stressful was this?","how stressful"),
-  (10, 4, 1, 20, NULL, "Did anything happen today that was enjoyable or felt good to you?","did anything enjoyable happen"),
-  (6, 4, 1, 21, 20, "If so, how enjoyable was this?","how enjoyable");
+  (10, 4, 1, 9, NULL, "Did you exercise today?","did you exercise"),
+  (8, 4, 1, 10, 9, "If yes, what type of exercise did you today?","type of exercise"),
+  
+  -- TODO - confirm w Nithya
+  (13, 4, 1, 11, 9, "If you exercised, for how many minutes did you exercise?","how many minutes did you exercise"),
+  --
+  
+  (14, 4, 1, 12, 9, "If you exercised, did you enjoy exercising?","exercise enjoyment"),
+  (11, 4, 1, 13, 9, "If you didn't exercise, why not? (lack of time, lack of self-discipline, fatigue, procrastination, lack of interest, family work or responsibilities,","exercise interference"),
+  
+  (9, 4, 1, 14, NULL, "How many alcoholic beverages did you have today?","number of alcoholic beverages"),
+  (9, 4, 1, 15, NULL, "How many caffeinated beverages did you have today?","number of caffeinated beverages"),
+  (10, 4, 1, 16, NULL, "Did you have any high sugar food or drinks today? (soft drinks, candy, etc)","sugary food or drinks"),
+  (10, 4, 1, 17, NULL, "Did anything happen today that was stressful or difficult for you?","did anything stressful happen"),
+  (14, 4, 1, 18, 17, "If so, how stressful was this?","how stressful"),
+  (10, 4, 1, 19, NULL, "Did anything happen today that was enjoyable or felt good to you?","did anything enjoyable happen"),
+  (14, 4, 1, 20, 19, "If so, how enjoyable was this?","how enjoyable");
   
