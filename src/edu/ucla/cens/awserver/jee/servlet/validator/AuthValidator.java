c1125e30fac390cdb239a61479bcd09ebde874d3
Joshua Selsky <selsky@cens.ucla.edu>
2010-09-13 18:03:23 -0700

    phone authentication is now based around the generic AwDataServlet

---
+++ src/edu/ucla/cens/awserver/jee/servlet/validator/AuthValidator.java
@@ -0,0 +1,85 @@
+package edu.ucla.cens.awserver.jee.servlet.validator;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.log4j.Logger;
+
+/**
+ * 
+ * @author selsky
+ */
+public class AuthValidator extends AbstractHttpServletRequestValidator {
+	private static Logger _logger = Logger.getLogger(AuthValidator.class);
+	private List<String> _parameterList;
+	
+	/**
+	 * 
+	 */
+	public AuthValidator() {
+		_parameterList = new ArrayList<String>(Arrays.asList(new String[]{"p","u","ci"}));
+	}
+	
+	/**
+	 * 
+	 */
+	public boolean validate(HttpServletRequest httpServletRequest) {
+		Map<?,?> parameterMap = httpServletRequest.getParameterMap(); // String, String[]
+		
+		// Check for missing or extra parameters
+		
+		if(parameterMap.size() != _parameterList.size()) {
+			_logger.warn("an incorrect number of parameters was found on sensor authentication: " + parameterMap.size());
+			return false;
+		}
+		
+		// Check for duplicate parameters
+		
+		Iterator<?> iterator = parameterMap.keySet().iterator();
+		
+		while(iterator.hasNext()) {
+			String key = (String) iterator.next();
+			String[] valuesForKey = (String[]) parameterMap.get(key);
+			
+			if(valuesForKey.length != 1) {
+				_logger.warn("an incorrect number of values (" + valuesForKey.length + ") was found for parameter " + key);
+				return false;
+			}
+		}
+		
+		// Check for parameters with unknown names
+		
+		iterator = parameterMap.keySet().iterator(); // there is no way to reset the iterator so just obtain a new one
+		
+		while(iterator.hasNext()) {
+			String name = (String) iterator.next();
+			if(! _parameterList.contains(name)) {
+			
+				_logger.warn("an incorrect parameter name was found: " + name);
+				return false;
+			}
+		}
+		
+		String u = (String) httpServletRequest.getParameter("u");
+		String p = (String) httpServletRequest.getParameter("p");
+		String ci = (String) httpServletRequest.getParameter("ci");
+		
+		// Check for abnormal lengths (buffer overflow attack)
+		// 50 is an arbitrary number for length, but it would be very strange
+		// 180 characters for the password would represent a 60 character password with every character URL encoded
+		
+		if(greaterThanLength("user", "u", u, 50) 
+			|| greaterThanLength("password", "p", p, 180) 
+			|| greaterThanLength("client", "ci", ci, 100)) { 
+			return false;
+		}
+		
+		return true;
+	}
+	
+}
