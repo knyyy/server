d0e21e67a79f9b225d3d945c80c07d70e5c2a932
Joshua Selsky <selsky@cens.ucla.edu>
2010-11-05 11:15:48 -0700

    added validation checks against users and campaigns for the data point api. also, cleaned up web.xml for handling of 404 and 500 responses.

--- src/edu/ucla/cens/awserver/dao/FindAllCampaignsForUserDao.java
+++ src/edu/ucla/cens/awserver/dao/FindAllCampaignsForUserDao.java
@@ -12,24 +12,35 @@ import edu.ucla.cens.awserver.request.AwRequest;
  */
 public class FindAllCampaignsForUserDao extends AbstractDao {
 	private static Logger _logger = Logger.getLogger(FindAllCampaignsForUserDao.class);
-	private String _sql = "SELECT c.name " +
-			              "FROM campaign c, user_role_campaign urc " +
-			              "WHERE urc.campaign_id = c.id AND urc.user_id = ?";
+	private boolean _useLoggedInUser;
 	
-	public FindAllCampaignsForUserDao(DataSource dataSource) {
+	private String _sql = "SELECT DISTINCT c.name " +
+			              "FROM campaign c, user_role_campaign urc, user u " +
+			              "WHERE urc.campaign_id = c.id AND urc.user_id = u.id AND u.login_id = ?";
+	
+	/**
+	 * @param useLoggedInUser if true, the logged in user's user name will be used in the query. if false, the user name request
+	 * parameter will be used (e.g., the data point API). 
+	 */
+	public FindAllCampaignsForUserDao(DataSource dataSource, boolean useLoggedInUser) {
 		super(dataSource);
+		_useLoggedInUser = useLoggedInUser;
 	}
 	
 	@Override
 	public void execute(AwRequest awRequest) {
 		try {
 			awRequest.setResultList(
-				getJdbcTemplate().query(_sql, new Object[]{awRequest.getUser().getId()}, new SingleColumnRowMapper())
+				getJdbcTemplate().query(
+					_sql, 
+					_useLoggedInUser ? new Object[] { awRequest.getUser().getUserName() } 
+									 : new Object[] { awRequest.getUserNameRequestParam() },
+					new SingleColumnRowMapper())
 			);
 		}	
 		catch (org.springframework.dao.DataAccessException dae) {
 			_logger.error("a DataAccessException occurred when running the following sql '" + _sql + "' with the parameter"
-				+ awRequest.getUser().getId(), dae);
+				+ (_useLoggedInUser ? awRequest.getUser().getUserName() : awRequest.getUserNameRequestParam()), dae);
 			throw new DataAccessException(dae);
 		}
 	}
--- src/edu/ucla/cens/awserver/jee/servlet/validator/DataPointQueryValidator.java
+++ src/edu/ucla/cens/awserver/jee/servlet/validator/DataPointQueryValidator.java
@@ -16,7 +16,7 @@ import org.apache.log4j.Logger;
  * @author selsky
  */
 public class DataPointQueryValidator extends AbstractHttpServletRequestValidator {
-	private static Logger _logger = Logger.getLogger(EmaQueryValidator.class);
+	private static Logger _logger = Logger.getLogger(DataPointQueryValidator.class);
 	private List<String> _parameterList;
 	
 	/**
@@ -33,10 +33,14 @@ public class DataPointQueryValidator extends AbstractHttpServletRequestValidator
 		Map<?,?> parameterMap = httpServletRequest.getParameterMap(); // String, String[]
 		
 		// Check for missing or extra parameters
-		// The "u" param is optional
-		if((parameterMap.size() != _parameterList.size()) && (parameterMap.size() != _parameterList.size() - 1)) {
-			_logger.warn("an incorrect number of parameters was found for an EMA viz query: " + parameterMap.size());
-			return false;
+		// The "u" param is optional in the special case where the query is one of the upload stat queries and the currently
+		// logged in user is a researcher or an admin
+		if(parameterMap.size() != _parameterList.size()) {
+			if(null != httpServletRequest.getParameter("u")) { // some other required parameter is missing
+				
+				_logger.warn("an incorrect number of parameters was found for an data point query: " + parameterMap.size());
+				return false;
+			}
 		}
 		
 		// ------- This could all be moved to a separate class
@@ -88,9 +92,10 @@ public class DataPointQueryValidator extends AbstractHttpServletRequestValidator
 		   || greaterThanLength("campaignVersion", "cv", cv, 250)
 		   || greaterThanLength("client", "ci",ci, 500)		   
 		   || greaterThanLength("authToken", "t", t, 50)
-  		   || greaterThanLength("dataPointId", "i", i, 250)
+  		   || greaterThanLength("dataId", "i", i, 250)
 		   || greaterThanLength("userName", "u", u, 75)) {
 			
+			_logger.warn("found an input parameter that exceeds its allowed length");
 			return false;
 		}
 		
