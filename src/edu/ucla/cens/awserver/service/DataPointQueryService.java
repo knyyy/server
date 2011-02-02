6b08eb708651c22c1abfd6a3ca890d595cefc40e
Joshua Selsky <selsky@cens.ucla.edu>
2011-02-01 19:16:33 -0800

    refactor and bugfix for handling multi-choice output properly

--- src/edu/ucla/cens/awserver/service/DataPointQueryService.java
+++ src/edu/ucla/cens/awserver/service/DataPointQueryService.java
@@ -32,22 +32,17 @@ public class DataPointQueryService extends AbstractDaoService {
 	}
 	
 	/**
-	 * Retrieves data points and creates a result list of DataPointQueryResults.
-	 * 
-	 * TODO - this method has lots of redundancy that could be cleaned up.
+	 * Retrieves data points and creates a result list of DataPointQueryResults. The data points are responses for promptIds from a 
+	 * campaign's XML configuration. In addition to looking up the prompt responses for each promptId, the metadata responses
+	 * (displayType == metadata) from the same survey as each prompt response are also retrieved.   
 	 */
 	@Override
 	public void execute(AwRequest awRequest) {
 		DataPointQueryAwRequest req = (DataPointQueryAwRequest) awRequest;
-		
-		// 1. Retrieve metadata data points from the config cache for the survey that wraps the prompt id being queried
-		// Metadata data points are sent with each data point from a particular survey.
-		
 		CampaignNameVersion cnv = new CampaignNameVersion(req.getCampaignName(), req.getCampaignVersion());
 		
-		// TODO - what if the end user has selected a metadata data point? it means there will be redundancy between the 
-		// dataPointId and the metadataPromptIds. not really the end of world, just sloppy. Clients should disallow it.
-		
+		// 1. Find the metadata data point ids: these are ids with a display type of metadata from the same survey as the prompt 
+		// ids that are being queried against. The metadata values are added to the results list as distinct query results.
 		Configuration config = (Configuration) _configurationCacheService.lookup(cnv);
 		List<String> metadataPromptIds = new ArrayList<String>();
 		String[] dataPointIds = req.getDataPointIds();
@@ -61,12 +56,12 @@ public class DataPointQueryService extends AbstractDaoService {
 		
 		req.setMetadataPromptIds(metadataPromptIds);	
 		
-		// 2. Pass those ids to the DAO
+		// 2. Pass all ids to the data access object and run the query
 		getDao().execute(req);
 		
 		// 3. Post-process
 		// Label the data points that are metadata
-		// Set the displayLabel and the unit from the survey config
+		// Set the displayLabel and the unit from the survey config if either are available
 		List<?> results = req.getResultList();
 		int numberOfResults = results.size();
 		
@@ -89,59 +84,11 @@ public class DataPointQueryService extends AbstractDaoService {
 				
 				if(PromptTypeUtils.isSingleChoiceType(result.getPromptType())) {
 					
-					String value = config.getValueForChoiceKey(
-						result.getSurveyId(), result.getRepeatableSetId(), result.getPromptId(), String.valueOf(result.getResponse())
-					);
-					
-					if(null != value) {
-						
-						result.setDisplayValue(value);
-						
-					} else { 
-						
-						result.setDisplayValue(result.getResponse());
-						
-					}
+					setDisplayValueFromSingleChoice(result, config, true);	
 					
 				} else if(PromptTypeUtils.isMultiChoiceType(result.getPromptType())) {
 					
-					JSONArray responseArray = JsonUtils.getJsonArrayFromString(String.valueOf(result.getResponse()));
-					
-					if(null != responseArray) {
-					
-						JSONArray valueArray = new JSONArray();
-						int length = responseArray.length();
-						
-						for(int j = 0; j < length; j++) {
-							
-							Object value = config.getValueForChoiceKey(result.getSurveyId(), result.getRepeatableSetId(), 
-								result.getPromptId(), JsonUtils.getStringFromJsonArray(responseArray, j)
-							);
-							
-							if(null == value) {
-								
-								break;
-								
-							} else {
-								
-								valueArray.put(value);
-							}
-						}
-						
-						if(valueArray.length() != length) {
-							
-							result.setDisplayValue(valueArray);
-							
-						} else {
-							
-							result.setDisplayValue(result.getResponse());
-						}
-					
-					} else {
-						
-						result.setDisplayValue(result.getResponse());
-						
-					}
+					setDisplayValueFromMultiChoice(result, config, true);
 					
 				} else { 
 						
@@ -156,57 +103,11 @@ public class DataPointQueryService extends AbstractDaoService {
 				
 				if(PromptTypeUtils.isSingleChoiceType(result.getPromptType())) {
 				
-					String value = config.getValueForChoiceKey(
-						result.getSurveyId(), result.getPromptId(), String.valueOf(result.getResponse())
-					);
-					
-					if(null != value) {
-						
-						result.setDisplayValue(value);
-						
-					} else {
-
-						result.setDisplayValue(result.getResponse());
-					}
+					setDisplayValueFromSingleChoice(result, config, false);
 				
 				} else if (PromptTypeUtils.isMultiChoiceType(result.getPromptType())) {
 					
-					JSONArray responseArray = JsonUtils.getJsonArrayFromString(String.valueOf(result.getResponse()));
-					
-					if(null != responseArray) {
-					
-						JSONArray valueArray = new JSONArray();
-						int length = responseArray.length();
-						
-						for(int j = 0; j < length; j++) {
-							
-							Object value = config.getValueForChoiceKey(result.getSurveyId(), result.getRepeatableSetId(), 
-								result.getPromptId(), JsonUtils.getStringFromJsonArray(responseArray, j)
-							);
-							
-							if(null == value) {
-
-								break;
-								
-							} else {
-								
-								valueArray.put(value);
-							}
-						}
-						
-						if(valueArray.length() != length) {
-							
-							result.setDisplayValue(valueArray);
-							
-						} else {
-							
-							result.setDisplayValue(result.getResponse());
-						}
-
-					} else { 
-						
-						result.setDisplayValue(result.getResponse());
-					}
+					setDisplayValueFromMultiChoice(result, config, false);
 										
 				} else {
 					
@@ -217,6 +118,66 @@ public class DataPointQueryService extends AbstractDaoService {
 		
 		if(_logger.isDebugEnabled()) {
 			_logger.debug(req.getResultList());
+		}
+	}
+	
+	private void setDisplayValueFromSingleChoice(DataPointQueryResult result, Configuration config, boolean isRepeatableSetItem) {
+		String value = null;
+		
+		if(isRepeatableSetItem) {
+			value = config.getValueForChoiceKey(
+				result.getSurveyId(), result.getRepeatableSetId(), result.getPromptId(), String.valueOf(result.getResponse())
+			);
+		} else {
+			value = config.getValueForChoiceKey(
+				result.getSurveyId(), result.getPromptId(), String.valueOf(result.getResponse())
+			);
+		}
+		
+		if(null != value) {
+			result.setDisplayValue(value);
+		} else {
+			result.setDisplayValue(result.getResponse());
+		}
+	}
+	
+	private void setDisplayValueFromMultiChoice(DataPointQueryResult result, Configuration config, boolean isRepeatableSetItem) {
+	
+		JSONArray responseArray = JsonUtils.getJsonArrayFromString(String.valueOf(result.getResponse()));
+		
+		if(null != responseArray) {
+			JSONArray valueArray = new JSONArray();
+			int length = responseArray.length();
+			
+			for(int j = 0; j < length; j++) {
+				
+				Object value = null;
+				
+				if(isRepeatableSetItem) {
+					value = config.getValueForChoiceKey(result.getSurveyId(), result.getRepeatableSetId(), 
+						result.getPromptId(), JsonUtils.getStringFromJsonArray(responseArray, j)
+					);
+				} else {
+					value = config.getValueForChoiceKey(
+						result.getSurveyId(), result.getPromptId(), JsonUtils.getStringFromJsonArray(responseArray, j)
+					);
+				}
+				
+				if(null == value) {
+					break;
+				} else {
+					valueArray.put(value);
+				}
+			}
+			
+			if(valueArray.length() == length) {
+				result.setDisplayValue(valueArray);
+			} else {				
+				result.setDisplayValue(result.getResponse());
+			}
+		
+		} else {
+			result.setDisplayValue(result.getResponse());
 		}
 	}
 }
